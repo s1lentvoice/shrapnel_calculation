9243843b6580df24a1660d82bf420cd87a8246d3
s1lentvoice <ant1psr@gmail.com>
2023-04-09 22:02:52 +0300

    ver 1.01

--- GraduateWork/main.cpp
+++ GraduateWork/main.cpp
@@ -16,6 +16,7 @@
 int main() {
 	std::vector <raw_data> data;
 	std::vector <fragment> fragments;
+	std::vector <raw_data> raw_dataset;
 	missile_data missile;
 	target_data target;
 	raw_data temp;
@@ -30,79 +31,84 @@ int main() {
 	std::string filename = "1.txt";
 	std::ofstream out("output.txt");
 	
+	raw_dataset = MakeRawData(filename);
 
-	
-	temp = MakeRawData(filename);
+	for (int k = 0; k < 100; k++) {
+		std::cout << "[" << k << "] set:" << std::endl << std::endl;
 
-	missile = MakeDataMissile(temp);
-	base_x = MakeRotation(base_x, missile.yaw_angle, missile.pitch_angle);
-	target = ModifyCoords(MakeDataTarget(temp), missile);
-	surface TargetSurfaceZX, TargetSurfaceXY, TargetSurfaceYZ;
-	
-	TargetSurfaceZX = MakeTargetSurface(target.basis_y, target.coord_n_obj);
-	TargetSurfaceYZ = MakeTargetSurface(target.basis_x, target.coord_n_obj);
-	TargetSurfaceXY = MakeTargetSurface(target.basis_z, target.coord_n_obj);
+		temp = raw_dataset[k];
 
-	std::cout << TargetSurfaceZX.A << "x + " << TargetSurfaceZX.B << "y + " << TargetSurfaceZX.C << "z + " << TargetSurfaceZX.D << " = 0" << std::endl;
-	
-	std::cout << TargetSurfaceXY.A << "x + " << TargetSurfaceXY.B << "y + " << TargetSurfaceXY.C << "z + " << TargetSurfaceXY.D << " = 0" << std::endl;
-	fragments = MakeFragments(missile, target, FragmentsInitialSpeed, FragmentsQuantity);
-	int total_near = 0;
-	double avg_near = 0.0;
-	int counter = 1;
-	int counter_near = 0;
-	int counter_miss = 0;
-	int counter_fragments_to_surface = 0;
+		missile = MakeDataMissile(temp);
+		base_x = MakeRotation(base_x, missile.yaw_angle, missile.pitch_angle);
+		target = ModifyCoords(MakeDataTarget(temp), missile);
+		surface TargetSurfaceZX, TargetSurfaceXY, TargetSurfaceYZ;
 
+		TargetSurfaceZX = MakeTargetSurface(target.basis_y, target.coord_n_obj);
+		TargetSurfaceYZ = MakeTargetSurface(target.basis_x, target.coord_n_obj);
+		TargetSurfaceXY = MakeTargetSurface(target.basis_z, target.coord_n_obj);
 
-	progressbar bar(100);
-	bar.set_todo_char(" ");
-	bar.set_done_char("#");
+		std::cout << TargetSurfaceZX.A << "x + " << TargetSurfaceZX.B << "y + " << TargetSurfaceZX.C << "z + " << TargetSurfaceZX.D << " = 0" << std::endl;
 
-	for (int j = 0; j < 10000; j++) {
+		std::cout << TargetSurfaceXY.A << "x + " << TargetSurfaceXY.B << "y + " << TargetSurfaceXY.C << "z + " << TargetSurfaceXY.D << " = 0" << std::endl;
 		fragments = MakeFragments(missile, target, FragmentsInitialSpeed, FragmentsQuantity);
-		counter_near = 0;
-		double distance = 0.0;
-		coordinates point;
+		int total_near = 0;
+		double avg_near = 0.0;
+		int counter = 1;
+		int counter_near = 0;
+		int counter_miss = 0;
+		int counter_fragments_to_surface = 0;
 
-		for (unsigned int i = 0; i < fragments.size(); ++i) {
-			if (fragments[i].hitZX && !fragments[i].calculated) {
-				point = PointOfImpact(fragments[i], TargetSurfaceZX);
-				//distance = PointsDistance(point, target.coord_n_obj);
 
-				//out << "(" << point.z << ", " << point.x << ")" << std::endl;
-				if (HitTargetZX(target, point)) {
-					counter_near++;
-					fragments[i].calculated = true;
+		progressbar bar(100);
+		bar.set_todo_char(" ");
+		bar.set_done_char("#");
+
+		for (int j = 0; j < 10000; j++) {
+			fragments = MakeFragments(missile, target, FragmentsInitialSpeed, FragmentsQuantity);
+			counter_near = 0;
+			double distance = 0.0;
+			coordinates point;
+
+			for (unsigned int i = 0; i < fragments.size(); ++i) {
+				if (fragments[i].hitZX && !fragments[i].calculated) {
+					point = PointOfImpact(fragments[i], TargetSurfaceZX);
+					//distance = PointsDistance(point, target.coord_n_obj);
+
+					//out << "(" << point.z << ", " << point.x << ")" << std::endl;
+					if (HitTargetZX(target, point)) {
+						counter_near++;
+						fragments[i].calculated = true;
+					}
+				}
+				if (fragments[i].hitXY && !fragments[i].calculated) {
+					point = PointOfImpact(fragments[i], TargetSurfaceXY);
+					//distance = PointsDistance(point, target.coord_n_obj);
+
+					//out << "(" << point.z << ", " << point.x << ")" << std::endl;
+					if (HitTargetXY(target, point)) {
+						counter_near++;
+						fragments[i].calculated = true;
+					}
 				}
 			}
-			if (fragments[i].hitXY && !fragments[i].calculated) {
-				point = PointOfImpact(fragments[i], TargetSurfaceXY);
-				//distance = PointsDistance(point, target.coord_n_obj);
 
-				//out << "(" << point.z << ", " << point.x << ")" << std::endl;
-				if (HitTargetXY(target, point)) {
-					counter_near++;
-					fragments[i].calculated = true;
-				}
+			//std::cout << "[" << j + 1 << "] fragments to target surface: " << counter << std::endl
+				//<< "[" << j + 1 << "] fragments hit the target: " << counter_near << std::endl << std::endl;
+			total_near += counter_near;
+
+			if (counter_near == 0)
+				counter_miss++;
+			if (counter == 100) {
+				counter = 0;
+				bar.update();
 			}
+			counter++;
 		}
-
-		//std::cout << "[" << j + 1 << "] fragments to target surface: " << counter << std::endl
-			//<< "[" << j + 1 << "] fragments hit the target: " << counter_near << std::endl << std::endl;
-		total_near += counter_near;
-
-		if (counter_near == 0)
-			counter_miss++;
-		if (counter == 100) {
-			counter = 0;
-			bar.update();
-		}
-		counter++;
+		avg_near = total_near / 10000;
+		std::cout << std::endl << std::endl << "avg fragments hit the target: " << avg_near << std::endl << std::endl;
+		std::cout << "missed: " << counter_miss << std::endl << std::endl;
+		std::cout << "avg to surface: " << counter_fragments_to_surface / 10000 << std::endl << std::endl;
 	}
-	avg_near = total_near / 10000;
-	std::cout << std::endl << std::endl << "avg fragments hit the target: " << avg_near << std::endl << std::endl;
-	std::cout << "missed: " << counter_miss << std::endl << std::endl;
-	std::cout << "avg to surface: " << counter_fragments_to_surface / 10000 << std::endl << std::endl;
+	
 	//to_do_next
 }
