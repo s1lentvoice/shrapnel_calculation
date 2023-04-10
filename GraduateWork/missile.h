e1762a1779eff49cb3b9103e249db14dc23f4f0b
s1lentvoice <ant1psr@gmail.com>
2023-04-10 17:26:51 +0300

    ver 1.03

--- GraduateWork/missile.h
+++ GraduateWork/missile.h
@@ -331,6 +331,82 @@ bool HitTargetBodyXY(target_data T, coordinates point, coordinates base_x, coord
 	return res;
 }
 
+bool HitTargetEmpennageXYupper(target_data T, coordinates point, coordinates base_x, coordinates base_y) {
+	bool res;
+	std::vector <coordinates> polygon_vertices;
+	coordinates temp;
+
+	temp.x = 0.0 - base_y.x * 3.0 - base_x.x * 3.0;
+	temp.y = 0.0 + base_x.y * 0.0 + base_y.y * 0.0;
+	temp = MakeRotation(temp, 0.0, T.pitch_obj);
+	temp.x += T.coord_n_obj.x;
+	temp.y += T.coord_n_obj.y;
+	polygon_vertices.push_back(temp);
+
+	temp.x = 0.0 - base_x.x * 3.6 - base_y.x * 3.6;
+	temp.y = 0.0 + base_x.y * 2.01 + base_y.y * 2.01;
+	temp = MakeRotation(temp, 0.0, T.pitch_obj);
+	temp.x += T.coord_n_obj.x;
+	temp.y += T.coord_n_obj.y;
+	polygon_vertices.push_back(temp);
+
+	temp.x = 0.0 - base_x.x * 4.1 - base_y.x * 4.1;
+	temp.y = 0.0 + base_x.y * 2.01 + base_y.y * 2.01;
+	temp = MakeRotation(temp, 0.0, T.pitch_obj);
+	temp.x += T.coord_n_obj.x;
+	temp.y += T.coord_n_obj.y;
+	polygon_vertices.push_back(temp);
+
+	temp.x = 0.0 - base_x.x * 4.1 - base_y.x * 4.1;
+	temp.y = 0.0 + base_x.y * 0.0 + base_y.y * 0.0;
+	temp = MakeRotation(temp, 0.0, T.pitch_obj);
+	temp.x += T.coord_n_obj.x;
+	temp.y += T.coord_n_obj.y;
+	polygon_vertices.push_back(temp);
+
+	res = CrossingNumberAlgoXY(polygon_vertices, point);
+
+	return res;
+}
+
+bool HitTargetEmpennageXYlower(target_data T, coordinates point, coordinates base_x, coordinates base_y) {
+	bool res;
+	std::vector <coordinates> polygon_vertices;
+	coordinates temp;
+
+	temp.x = 0.0 - base_y.x * 3.0 - base_x.x * 3.0;
+	temp.y = 0.0 - base_x.y * 0.625 - base_y.y * 0.625;
+	temp = MakeRotation(temp, 0.0, T.pitch_obj);
+	temp.x += T.coord_n_obj.x;
+	temp.y += T.coord_n_obj.y;
+	polygon_vertices.push_back(temp);
+
+	temp.x = 0.0 - base_x.x * 3.6 - base_y.x * 3.6;
+	temp.y = 0.0 - base_x.y * 1.6 - base_y.y * 1.6;
+	temp = MakeRotation(temp, 0.0, T.pitch_obj);
+	temp.x += T.coord_n_obj.x;
+	temp.y += T.coord_n_obj.y;
+	polygon_vertices.push_back(temp);
+
+	temp.x = 0.0 - base_x.x * 4.1 - base_y.x * 4.1;
+	temp.y = 0.0 - base_x.y * 1.6 - base_y.y * 1.6;
+	temp = MakeRotation(temp, 0.0, T.pitch_obj);
+	temp.x += T.coord_n_obj.x;
+	temp.y += T.coord_n_obj.y;
+	polygon_vertices.push_back(temp);
+
+	temp.x = 0.0 - base_x.x * 4.1 - base_y.x * 4.1;
+	temp.y = 0.0 - base_x.y * 0.625 - base_y.y * 0.625;
+	temp = MakeRotation(temp, 0.0, T.pitch_obj);
+	temp.x += T.coord_n_obj.x;
+	temp.y += T.coord_n_obj.y;
+	polygon_vertices.push_back(temp);
+
+	res = CrossingNumberAlgoXY(polygon_vertices, point);
+
+	return res;
+}
+
 bool HitTargetZX(target_data T, coordinates point) {
 	bool res;
 	std::vector <coordinates> points;
@@ -371,6 +447,13 @@ bool HitTargetXY(target_data T, coordinates point) {
 	base_y.z = 0.0;
 
 	res = HitTargetBodyXY(T, point, base_x, base_y);
+
+	if (!res)
+		HitTargetEmpennageXYupper(T, point, base_x, base_y);
+
+	if (!res)
+		HitTargetEmpennageXYlower(T, point, base_x, base_y);
+
 	return res;
 }
 
